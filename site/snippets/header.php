1253d7f4cc15df50612aed2ace57c4da3708fd6f
Jon Schuster <aonmedia@live.com>
2017-08-15 16:54:09 -0400

    added manifest

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -8,6 +8,7 @@
     <meta name="description" content="<?= $site->description()->html() ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#ca281b" />
+    <link rel="manifest" href="manifest.json">
     <?= css('assets/css/index.css') ?>
 
     <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js" defer></script><![endif]-->
